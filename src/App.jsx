519034ed5f11e530ddb2b8d2b47f6afacd6ae1b1
festus-sulumeti <sulumetifestus@outlook.com>
2024-01-19 19:31:31 +0300

    The project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-const App = () => {
-    <main className="relative">
-      Nav
-      <section className="x1:padding-1
-        wide:padding-r pdding-b
-      ">
-        Hero
-      </section>
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="x1:padding-1
+      wide:padding-r pdding-b
+    ">
+      Hero
+    </section>
 
-      <section className="padding"></section>
-    </main>
-}
+    <section className="padding"></section>
+  </main>
+)
+
+export default App;
