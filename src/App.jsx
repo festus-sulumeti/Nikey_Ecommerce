f5ecf51d6621b16c74c7530d7b93d108e75c1fe6
festus-sulumeti <sulumetifestus@outlook.com>
2024-01-20 09:19:00 +0300

    THe files

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,27 @@ const App = () => (
       Hero
     </section>
 
-    <section className="padding"></section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      Special offer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding">
+      Footer
+    </section>
   </main>
 )
 
